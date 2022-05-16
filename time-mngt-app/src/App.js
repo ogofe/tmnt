b490de0a1e79233c80a01b4114cad732e8e7a61d
ogofe <7thogofe@gmail.com>
2022-05-16 04:29:55 +0100

    first-release

---
+++ time-mngt-app/src/App.js
@@ -0,0 +1,119 @@
+import React, {useState, useRef} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ActivityForm} from './components';
+
+const App =()=> {
+  const [activities, setActivities] = useState([]);
+  const totalHours = 168;
+  const addForm = useRef();
+  const [time, setTime] = useState("weekly")
+  const [freeTime, setFreeTime] = useState(totalHours)
+  const [showResult, setShowResult] = useState(false)
+
+  function addActivity(data){
+    setActivities([...activities, data])
+  }
+
+  function getFreeTime(){
+    if (activities.length < 1){
+      return
+    }
+    let _freeTime = totalHours;
+    for (let act of activities){
+      let cost = act.timeCost;
+      if (act.time === "mins"){
+        cost = cost / 60
+      }
+      setFreeTime(_freeTime - (act.freq * cost))
+    }
+    setShowResult(true);
+  }
+
+  function showAddForm(e){
+    document.querySelector('.overlay').classList.add('show')
+    document.querySelector('#activity-form').classList.remove('hide')
+  }
+
+  function parseTime(_time){
+    if (time === "daily"){
+      _time = _time / 7
+    }
+
+    let hours = Number(String(_time).split('.')[0])
+    let _mins = ("0." + String(_time).split('.')[1])
+    let mins = Number(_mins)
+    mins = Math.round(mins * 60)    
+    return {mins, hours}
+  }
+
+  function removeItem(item){
+    let list = Array.from(activities), idx = list.indexOf(item);
+    list.splice(idx, 1)
+    setActivities(list)
+  }
+
+  return (
+    <div className="">
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4> Time Management Tool - Free time finder</h4>
+      </header>
+
+      <div className="p-2">
+        <div className="flexbox" style={{borderBottom: '1px solid grey'}}>
+          <h4 className="mt-0">  Total Hours </h4>
+          <h4 className="mt-0"> {totalHours} hours </h4>
+        </div>
+
+        <div>
+          <div className="flexbox">
+            <h3 style={{textAlign: 'left', color: 'grey'}}> Activities </h3>
+            <button onClick={showAddForm}> + Add </button>
+          </div>
+
+          <ul className="activities">
+            {activities.map((item, idx) => 
+              <div key={idx} className="flexbox activity">
+                <li className="">
+                    <h4 className="act"> {item.activity} </h4>
+                    <p className="time">
+                     {item.timeCost} {item.time} x {item.freq} day{item.freq > 1 && "s"}
+                    </p>
+                </li>
+
+                <button onClick={e => removeItem(item)}>
+                  &times;
+                </button>
+              </div>
+            )}
+          </ul>
+         <ActivityForm onSubmit={addActivity}/> 
+        </div>
+
+        <button onClick={getFreeTime}> Calculate free time </button>
+
+        {showResult && 
+          <div className="result">
+            <select defaultValue={time} onInput={e => setTime(e.target.value)}>
+              <option> daily </option>
+              <option> weekly </option>
+            </select>
+            <h4> You have {parseTime(freeTime).hours} hours {parseTime(freeTime).mins || 0} mins free time {time}</h4>
+          </div>
+        }
+      </div>
+
+      <footer>
+        <p> Made with 💜 by <a className="app-link" href="https://jtogofe.netlify.app">Joel O. Tanko</a> </p>
+      </footer>
+    </div>
+
+
+    <div className="overlay"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ time-mngt-app/src/components.jsx
@@ -0,0 +1,112 @@
+import React, {useState, useRef} from 'react';
+
+
+export const ComboBox=({ onTextInput, addMissing, data, error, onSubmit, label, ...props })=>{
+  
+  const [matches, setMatches] = useState(data);
+  const filter = useRef()
+  window.filter = filter;
+
+  function filterList(query){
+    if(query === "") {
+      setMatches(data);
+      return
+    }
+    let list = matches.filter(item => (item.toLowerCase() === query.toLowerCase()))
+    if (!list.includes(query)){
+     list.push(query)
+    }
+    if (list.length > 0){
+      list = data
+    } 
+    setMatches(list);
+  }
+
+  return(
+    <div className={"combo-box" + (props?.className ? props.className : "")} {...props?.style}>
+      <p className="combo-label"> {label} </p>
+      <input disabled ref={filter}
+       className={"combo-filter" + (error ? "error" : "")} 
+      />
+      <ul className="combo-list">
+        {matches.map((item, idx) => 
+          <li key={idx} className="combo-item" onClick={e => {
+            filter.current.value = item
+            onTextInput(item)
+          }}> {item} </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export const ActivityForm = ({ onSubmit }) =>{
+  const [activity, setActivity] = useState(null);
+  const [timeCost, setTimeCost] = useState(null);
+  const [time, setTime] = useState("hours");
+  const [freq, setFreq] = useState(1);
+  const [error, setError] = useState(false);
+  const sampleActvities = [
+    "Chores", "Commute / Transportation", "Cooking",
+    "Eating", "Gaming", "Hangout with friends", 
+    "Reading","School", "Sleep", "Social media",
+    "Watching Television", "Work", "Workout"
+    ]
+
+  function addActivity(e){
+    e.preventDefault();
+    if (!activity){
+      setError(true);
+      window.alert("Please select an activity")
+      return
+    }
+    onSubmit({
+      activity,
+      timeCost,
+      freq,
+      time
+    })
+    e.target.reset();
+  }
+
+  function hideAddForm(e){
+    document.querySelector('.overlay').classList.remove('show');
+    document.querySelector('#activity-form').classList.add('hide');
+    document.querySelector('#form').reset();
+  }
+
+  return(
+    <div id="activity-form" className="hide">
+      <form method="post" id="form" onSubmit={addActivity}>
+        <ComboBox error={error} data={sampleActvities} addMissing={true} label="Activity" onTextInput={act => setActivity(act)} />
+        
+        <div className="flexbox p-1 pt-0 pb-0" style={{margin: '10px auto'}}>
+          <div className="div">
+            <span className="label">Daily occurence </span>
+            <input type="number" step="1" min="1" defaultValue="1" max="7" onInput={e => setFreq(e.target.value)}/>
+          </div>
+
+          <div className="div">
+            <span className="label">Time cost </span>
+            
+            <div className="flexbox">
+              <input required type="number" max="60" min="1" defaultValue="1" onInput={e => setTimeCost(e.target.value)} />
+            
+              <select defaultValue={time} className="time-unit" onInput={e => setTime(e.target.value)}>
+                <option> hours </option>
+                <option> mins </option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="flexbox p-2 pt-0 pb-0">
+          <button onClick={hideAddForm}> add </button>
+          <button type="button" onClick={hideAddForm}> Cancel </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default {ComboBox, ActivityForm}
